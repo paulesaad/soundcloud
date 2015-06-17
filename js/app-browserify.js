bb4851570f7808c94e0c71f63ee06110c05e4880
materialfuture <materialfuture@gmail.com>
2015-06-17 13:38:47 -0500

    js/ adds songs by genres, html/ moved the scripts to header, will probably change that back

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -2,12 +2,44 @@
 
 // es5 polyfills, powered by es5-shim
 require("es5-shim")
-// es6 polyfills, powered by babel
+    // es6 polyfills, powered by babel
 require("babel/register")
 
 var Promise = require('es6-promise').Promise,
-	$ = require('jquery'),
-	Backbone = require('backbone')
+    $ = require('jquery'),
+    Backbone = require('backbone')
+
+sc_url = `https://api.soundcloud.com/oauth2/token`
+    // artist_url = 
+
+
+// Have search route to or from "search_soundcloud"
+
+SC.initialize({
+    client_id: '5e3fe3759c70fe637cb15bab22e238e0'
+});
+
+$(document).ready(function() {
+    SC.get('/tracks', {
+        genres: '...'
+    }, function(tracks) {
+        $(tracks).each(function(index, track) {
+            $('#results').append($('<li></li>').html(track.title + ' - ' + track.genre));
+        });
+    });
+});
+
+/*
+	Change #results to perhaps another phrase, unless results would be "likes", "durration", etc.
+	Still need to test before confirming this.
+/*
+
+//HTML
+       // <ul id="results">
+
+       //  </ul>
+
+
 
 // -----------------------------------------------------
 
@@ -19,3 +51,4 @@ client_secret: '11c8901be7ac78174059b4fb464c5072'
 
 
 
+*/
