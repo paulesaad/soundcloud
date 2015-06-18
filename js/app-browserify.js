d3b8c1e46cdc7863d2a5c8fa63b1aaaf77450ea0
paulesaad <paulsaad.13@gmail.com>
2015-06-18 09:36:31 -0500

    backbone and react skeletal structure in place

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -7,27 +7,97 @@ require("babel/register")
 
 var Promise = require('es6-promise').Promise,
     $ = require('jquery'),
-    Backbone = require('backbone')
+    Backbone = require('backbone'),
+    React = require('react'),
+    sc_key = '5e3fe3759c70fe637cb15bab22e238e0',
+    sc_url = `https://api.soundcloud.com/tracks?client_id=${sc_key}`
 
-sc_url = `https://api.soundcloud.com/oauth2/token`
+
+SC.initialize({
+  client_id: 'ff508288606264cb4f1469b5ba4f23e8'
+})
+
+var SC_Collection = Backbone.Collection.extend({
+    query: '',
+
+    url: function(){
+        return `${sc_url}&q=${this.query}`
+    }
+})
+
+var collection = new SC_Collection()
+collection.fetch().then(() => {
+    console.log(collection.toJSON())
+})
+
+
+// var matt_playlist = playlists/4759628
+
+
+// class SC_Item extends React.Component {
+//     constructor(props){
+//         super(props)
+//     }
+
+//     render(){
+//         return(
+//             <div>hi</div>
+//         )
+//     }
+// }
+
+// class searchList extends React.Component {
+//     constructor(){
+//         super(props)
+//     }
+
+//     render(){
+
+//     }
+// }
+
+// class App extends React.Component {
+//     constructor(){
+//         super(props)
+//     }
+
+//     render(){
+
+//     }
+// }
+
+// React.render(<SC_Item tracks={SC_Collection} />, document.querySelector('.container'))
+
+
+
+
+
+
+
+
+
+
+
+
+
     // artist_url = 
 
 
 // Have search route to or from "search_soundcloud"
 
-SC.initialize({
-    client_id: '5e3fe3759c70fe637cb15bab22e238e0'
-});
+// SC.initialize({
+//     client_id: '5e3fe3759c70fe637cb15bab22e238e0'
+// });
 
-$(document).ready(function() {
-    SC.get('/tracks', {
-        genres: '...'
-    }, function(tracks) {
-        $(tracks).each(function(index, track) {
-            $('#results').append($('<li></li>').html(track.title + ' - ' + track.genre));
-        });
-    });
-});
+// $(document).ready(function() {
+//     SC.get('/tracks', {
+//         genres: '...'
+//     }, function(tracks) {
+//         $(tracks).each(function(index, track) {
+//             $('#results').append($('<li></li>').html(track.title + ' - ' + track.genre));
+//         });
+//     });
+// });
 
 /*
 	Change #results to perhaps another phrase, unless results would be "likes", "durration", etc.
@@ -43,12 +113,30 @@ $(document).ready(function() {
 
 // -----------------------------------------------------
 
-SC.initialize({
-  client_id: 'ff508288606264cb4f1469b5ba4f23e8'
-}
-
 client_secret: '11c8901be7ac78174059b4fb464c5072'
 
 
 
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
